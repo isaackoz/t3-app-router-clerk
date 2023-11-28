72e3c2c9a9cad8ada26ec23c3c7804983a63e969
Vittorio D'Alfonso <vittoriodalfonso07@gmail.com>
2023-11-28 11:06:48 +0100

    AddeD Framer Motion and Animation

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,69 +1,89 @@
+"use client";
 import { CreatePost } from "@/app/_components/create-post";
-import { api } from "@/trpc/server";
+import { api } from "@/trpc/react";
+import { AnimatePresence, motion } from "framer-motion";
 import {
   SignIn,
+  SignInButton,
   SignOutButton,
   SignedIn,
   SignedOut,
   currentUser,
+  useUser,
 } from "@clerk/nextjs";
 import Link from "next/link";
+import Balancer from "react-wrap-balancer";
 
-export default async function Home() {
-  const hello = await api.post.hello.query({ text: "from tRPC" });
-  const user = await currentUser();
+export default function Home() {
+  const { data: hello, isLoading } = api.post.hello.useQuery({
+    text: "from tRPC",
+  });
+
+  const { user, isLoaded } = useUser();
+
+  if (isLoading || !isLoaded) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <p>Next 14 App Router + tRPC + Clerk demo</p>
-        <div>
-          <SignedOut>
-            <span className="font-red-500">You are not signed in.</span>
-            <SignIn afterSignInUrl={"/"} />
-          </SignedOut>
-          <SignedIn>
-            <SignOutButton>Sign out &rarr;</SignOutButton>
-            <div className="flex flex-col">
-              <span className="text-green-500">You are signed in</span>{" "}
-              <ul>
-                <li>
-                  {" "}
-                  <span className="font-bold text-gray-400">
-                    Username:
-                  </span>{" "}
-                  {user?.username ?? "No username"}
-                </li>
-                <li>
-                  {" "}
-                  <span className="font-bold text-gray-400">Email:</span>{" "}
-                  {user?.emailAddresses[0]?.emailAddress}
-                </li>
-                <li>
-                  <span className="font-bold text-gray-400">Full name:</span>{" "}
-                  {`${user?.firstName} ${user?.lastName}`}
-                </li>
-              </ul>
-            </div>
-          </SignedIn>
+    <AnimatePresence>
+      <main className="flex min-h-screen w-full flex-col items-center justify-center pt-48">
+        <div className="z-10 min-h-[50vh] w-full max-w-4xl px-5 xl:px-0">
+          <motion.h1
+            className="animate-fade-up bg-gradient-to-br from-foreground to-muted-foreground bg-clip-text text-center text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-7xl/[5rem]"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ delay: 0.1, duration: 0.6 }}
+          >
+            <Balancer> T3 Template App</Balancer>
+          </motion.h1>
+          <motion.p
+            className="animate-fade-up mt-6 text-center text-muted-foreground/80 md:text-xl"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ delay: 0.2, duration: 0.6 }}
+          >
+            <Balancer>
+              This is a boilerplate for a fullstack Next.js app i have done
+              implementing different template around the web.
+            </Balancer>
+          </motion.p>
+          <div>
+            {user ? (
+              <div>
+                <p className="animate-fade-up text-muted-foreground/800 mt-6 text-center md:text-xl">
+                  Logged in as {user.fullName}
+                </p>
+                <SignOutButton>Sign out</SignOutButton>
+              </div>
+            ) : (
+              <div className="flex flex-col items-center justify-center gap-3">
+                <p className=" mt-6 text-center text-black text-muted-foreground/80  md:text-xl">
+                  You are not logged in
+                </p>
+                <SignInButton>
+                  <button className="rounded-full bg-violet-400 p-3 font-bold text-slate-300">
+                    Sign in Now
+                  </button>
+                </SignInButton>
+              </div>
+            )}
+          </div>
+          <div className="mt-3 flex flex-col items-center">
+            <p className="text-2xl text-black">
+              {hello ? hello.greeting : "Loading tRPC query..."}
+            </p>
+          </div>
         </div>
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
-        </div>
-
-        <CrudShowcase />
-      </div>
-    </main>
+      </main>
+    </AnimatePresence>
   );
 }
 
 async function CrudShowcase() {
-  const latestPost = await api.post.getAllPosts.query();
+  const latestPost = api.post.getAllPosts.useQuery();
 
   return (
     <div className="w-full max-w-xs">
